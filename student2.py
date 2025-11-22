d0f7a332993403ccd1482d340c2a7094b1a0a979
techtayyab <ntayyab.tech@gmail.com>
2025-11-22 11:17:16 +0530

    Display user input values in student2.py
    Added print statements to display user input values.

--- student2.py
+++ student2.py
@@ -5,6 +5,10 @@ if len(sys.argv) == 3:
     name = sys.argv[1]
     rollno = sys.argv[2]
     print("user provided input values:")
+    print("script name:", script_name)
+    print("student name:", name)
+    print("roll number:", rollno)
+
 else:
     script_name = sys.argv[0]
     name = "tayyab"   # default value as string
