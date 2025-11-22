22b8c447852ef465af0df73bbd04ee2516473396
techtayyab <ntayyab.tech@gmail.com>
2025-11-22 10:55:21 +0530

    Update and rename check_num.py to student2.py

---
+++ student2.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) == 3:
+    script_name = sys.argv[0]
+    name = sys.argv[1]
+    rollno = sys.argv[2]
+    print("user provided input values:")
+else:
+    script_name = sys.argv[0]
+    name = "tayyab"   # default value as string
+    rollno = "100"    # default value as string
+    print("no input given - using default values:")
+    print("script name:", script_name)
+    print("student name:", name)
+    print("roll number:", rollno)
